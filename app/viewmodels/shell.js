9362ee88df4ab9f878536c4cd90bc8a18635ca74
Lee Elenbaas <lee@elenbaas.org.il>
2014-04-21 08:43:44 +0300

    Style the shell.js to be a JS type, and provide data binding for the search phrase typed by the user

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -1,18 +1,26 @@
 ﻿define(['plugins/router', 'durandal/app'], function (router, app) {
-    return {
-        router: router,
-        search: function() {
-            //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
-            app.showMessage('Search not yet implemented...');
-        },
-        activate: function () {
-            router.map([
-                { route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
-                { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true }
-            ]).buildNavigationModel();
-            
-            return router.activate();
-        }
-    };
+	function Shell(){
+		this.router = router;
+		this.searchPhrase = '';
+	}
+	
+	function search() {
+		//It's really easy to show a message box.
+		//You can add custom options too. Also, it returns a promise for the user's response.
+		app.showMessage('Search not yet implemented... ' + this.searchPhrase); // Will provide a way to search trhou the views, and through the predefined games
+	}
+	
+	function activate(){
+		this.router.map([
+			{ route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
+			{ route: 'flickr', moduleId: 'viewmodels/flickr', nav: true }
+		]).buildNavigationModel();
+		
+		return this.router.activate();
+	}
+
+	Shell.prototype.search = search;
+	Shell.prototype.activate = activate;
+	
+    return Shell;
 });
